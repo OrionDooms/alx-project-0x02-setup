d072446cd6f8331f750bb744935da5840425a009
Orion <Orion45@dd.gmail.com>
2025-07-22 21:28:51 +0200

    Create a Reusable Card Component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -3,9 +3,11 @@ import { CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
     return (
-        <div className="bg-white rounded-lg shadow-lg p-6 m-4 max-x-md dark:bg-gray-800">
-            <h2 className="text-xl font-semibold mb-2  text-gray-900 dark:text-white">{title}</h2>
+        <div className="bg-white rounded-lg shadow-lg p-6 m-4 max-x-md dark:bg-gray-800 border w-72">
+            <img className="w-full h-40 object-cover mb-4 rounded" src="https://placehold.co/300x160" alt="Placeholder"/>
+            <p className="text-gray-900 text-lg font-semibold dark:text-white mb-2">{title}</p>
             <p className="text-gray-700 dark:text-gray-300">{content}</p>
+            
         </div>
     )
 }
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -3,9 +3,18 @@ import Card from "@/components/common/Card";
 
 const Home: React.FC = () => {
 	return (
-	<div className="Flex justify-center item-center h-screen">
-	<h1>Home page</h1>
-	<Card title="card1" content="info" />
+	<div className="justify-center items-center flex-col h-screen">
+		<h1 >Home page</h1>
+		<div className="grid grid-cols-3 space-y-5">
+            {
+            [1,2,3,4,5].map((title, index) => (
+                <Card
+                key={index}
+                title={`Title ${title}`} 
+                content={`Content for card ${index}`} />
+                ) )
+            }
+            </div>
 	</div>
 	);
 };

194c01f734206496d371f5b20d1b248e02e601ea
Orion <Orion45@dd.gmail.com>
2025-07-24 00:44:44 +0200

    Implement a Modal Component

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -2,3 +2,16 @@ export interface CardProps {
  title: string;
  content: string;   
 }
+
+export interface PostModalProps {
+ isOpen: boolean;
+ onClose: () => void;
+ onSubmit: (post: { title: string; content: string }) => void;
+}
+
+export interface ButtonProps {
+    label: string;
+    size: 'small' | 'medium' | 'large';
+    shape: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+    onClick?: () => void;
+}

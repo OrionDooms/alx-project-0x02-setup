c18a1e872af9fc724220fe2ce6841bf8fdf261da
Orion <Orion45@dd.gmail.com>
2025-07-27 15:25:02 +0200

    update Card component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { type CardProps } from "@/interfaces";
 
+
 const Card: React.FC<CardProps> = ({ title, content }) => {
     return (
         <div className="bg-white rounded-lg shadow-lg p-6 m-4 max-x-md dark:bg-gray-800 border w-72">
-            <img className="w-full h-40 object-cover mb-4 rounded" src="https://placehold.co/300x160" alt="Placeholder"/>
-            <p className="text-gray-900 text-lg font-semibold dark:text-white mb-2">{title}</p>
+            <h2 className="text-gray-900 text-lg font-semibold mb-2">{title}</h2>
             <p className="text-gray-700 dark:text-gray-300">{content}</p>
-            
         </div>
     )
 }
